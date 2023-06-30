c98ac34ea9e8a4e33f6bc40b611760e80741acb6
PiethroCesar <piethrocesar@gmail.com>
2023-06-30 11:36:05 -0300

    Timers adicionados

--- serial/Trabalho_1.py
+++ serial/Trabalho_1.py
@@ -8,8 +8,6 @@ import time
 in_folder = "../img_in/"
 out_folder = '../img_out'
 
-
-
 def imprime(img, modo='gray', vmin=0, vmax=255):
     plt.imshow(img, cmap=modo, vmin=vmin, vmax=vmax)
     plt.show()
@@ -17,13 +15,14 @@ def imprime(img, modo='gray', vmin=0, vmax=255):
 
 ##### item 1.1
 def item_1_1():
+    st = time.process_time()
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
     img = imageio.imread(arquivo_1)
 
     # imprime(img)
-    st = time.process_time()
     largura_1 = len(img)
 
     novo = [ img[x*int(largura_1/4):(x+1)*int(largura_1/4),y*int(largura_1/4):(y+1)*int(largura_1/4)]for x in range(4)for y in range(4)]
@@ -38,17 +37,20 @@ def item_1_1():
 
     # imprime(linha)
     linha = linha.astype(np.uint8)
-    et = time.process_time()
 
-    tt_time = et-st
-    print("Elapsed", tt_time, "seconds")
     imageio.imsave(out_folder+'/item_1_1_out.png', linha)
+
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_1")
 
 
 ###### item 1.2 Combinação de imagens
 
 def item_1_2():
-        
+    st = time.process_time()
+    
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -59,7 +61,6 @@ def item_1_2():
 
 
     img_2 = imageio.imread(arquivo_2)
-    st = time.process_time()
 
     f, axarr = plt.subplots(1,3) 
 
@@ -76,23 +77,26 @@ def item_1_2():
     saida = img_1*0.8+img_2*0.2
     axarr[2].imshow(saida, cmap='gray', vmin=0, vmax=255)
     saida = saida.astype(np.uint8)
-    et = time.process_time()
-    tt_time = et-st
-    print("Elapsed", tt_time, "seconds")
-    imageio.imsave(out_folder+'/item_1_2_out_c.png', saida)
 
     plt.show()
 
 
+
+    imageio.imsave(out_folder+'/item_1_2_out_c.png', saida)
+
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_2")
+
 ###### item 1.3 Transformação de intensidade
 
 def item_1_3():
-
+    st = time.process_time()
+    
     arquivo_1 = "city.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
     img_1 = imageio.imread(arquivo_1)
-    st = time.process_time()
 
 
     saida_1 = img_1.astype(np.int16)
@@ -124,9 +128,7 @@ def item_1_3():
     plt.imshow(saida_3, cmap='gray', vmin=0, vmax=255)
     plt.show()
     saida_3 = saida_3.astype(np.uint8)
-    et = time.process_time()
-    tt_time = et-st
-    print("Elapsed", tt_time, "seconds")
+
     imageio.imsave(out_folder+'/item_1_3_out_c.png', saida_3)
 
     
@@ -157,9 +159,16 @@ def item_1_3():
     imageio.imsave(out_folder+'/item_1_3_out_e.png', saida_5)
 
 
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_3")
+
+
 ###### item 1.4 Transformação de Cores
 
 def item_1_4():
+    st = time.process_time()
+
     arquivo_1 = "colorido.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -202,13 +211,17 @@ def item_1_4():
     saida_mono = saida_mono.astype(np.uint8)
     imageio.imsave(out_folder+'/item_1_4_out_monocromatica.png', saida_mono)
 
-
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_4")
 
 
 ###### item 1.5 Transformação de Brilho
 
 
 def item_1_5():
+    st = time.process_time()
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -256,8 +269,14 @@ def item_1_5():
     saida = saida.astype(np.uint8)
     imageio.imsave(out_folder+'/item_1_5_out_gamma_3_5.png', saida)
 
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_5")
+
 
 def item_1_6():
+    st = time.process_time()
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -309,6 +328,10 @@ def item_1_6():
     imageio.imsave(out_folder+'/item_1_6_out_4_niveis.png', saida_4)
     imageio.imsave(out_folder+'/item_1_6_out_2_niveis.png', saida_2)
 
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_6")
+
 
 
 def item_1_7_aux(img, i):
@@ -325,6 +348,8 @@ def item_1_7_aux(img, i):
 
 
 def item_1_7():
+    st = time.process_time()
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -333,7 +358,13 @@ def item_1_7():
     for i in range(8):
         img_1 = item_1_7_aux(img_1, i)
 
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_7")
+
 def item_1_8():
+    st = time.process_time()
+
     arquivo_1 = "baboon.png"#input("Deseje o nome do arquivo desejado:\n")
     arquivo_1 = in_folder+arquivo_1
 
@@ -409,15 +440,19 @@ def item_1_8():
     # imprime(saida_1)
     saida_1 = saida_1.astype(np.uint8)
     imageio.imsave(out_folder+'/../img_out/item_1_8_out_H1.png', saida_1)
+
+    et = time.process_time()
+    tt_time = et-st
+    print("Elapsed", tt_time, "seconds on item_8")
     
 item_1_1()
-# item_1_2()
-# item_1_3()
-# item_1_4()
-# item_1_5()
-# item_1_6()
-# item_1_7()
-# item_1_8()
+item_1_2()
+item_1_3()
+item_1_4()
+item_1_5()
+item_1_6()
+item_1_7()
+item_1_8()
 
 
 
